c5de216fff8320dcd9cbb075d125a0a68c6fd2cf
Jhonatan <jhojho2jogos456@gmail.com>
2025-08-05 13:54:26 -0300

    kernel Aldebaramm initial upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Aug 4 15:21:59 -03 2025"
+#define LINUX_COMPILE_BY "jhonatan"
+#define LINUX_COMPILE_HOST "NP270E5G-Linux"
+#define LINUX_COMPILER "gcc version 4.9.4 20151028 (prerelease) (Linaro GCC 4.9-2016.02) "
